e499f5b158a156a16ec667c7b7184793ea5baa9d
LucianoBrumer <lucianobrumer5@gmail.com>
2023-11-03 17:54:39 -0300

    jwt

--- utilities/jwt.php
+++ utilities/jwt.php
@@ -24,6 +24,24 @@ function generate_jwt($payload = [], $secret_key = "secret_key", $exp = 3600) {
 
 function verify_jwt($jwt, $secret_key = "secret_key"){
     $tokenParts = explode('.', $jwt);
-    $header = base64_decode($tokenParts[0]);
-    $payload = base64_decode($tokenParts[1]);
+
+    if (count($tokenParts) !== 3) return false;
+
+    $base64Header = $tokenParts[0];
+    $base64Payload = $tokenParts[1];
+    $signature = $tokenParts[2];
+
+    $header = json_decode(base64_decode($base64Header), true);
+    $payload = json_decode(base64_decode($base64Payload), true);
+
+    if (!$header || !$payload) return false;
+
+    if (empty($header['alg']) || $header['alg'] !== 'HS256') return false;
+
+    $expectedSignature = hash_hmac('sha256', "$base64Header.$base64Payload", $secret_key);
+    if ($signature !== $expectedSignature) return false;
+
+    if (isset($payload['exp']) && $payload['exp'] < time()) return false;
+
+    return $payload;
 }
