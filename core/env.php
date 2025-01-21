f6108eaa44be01e6a14e08926cc2639e5ffc2b93
lullaby6 <lucianobrumer5@gmail.com>
2025-01-21 09:30:30 -0300

    fix .env

--- core/env.php
+++ core/env.php
@@ -6,13 +6,15 @@ $env_file_path = $_SERVER["DOCUMENT_ROOT"] . "/.env";
 
 if (file_exists($env_file_path)) {
     $env_content = file_get_contents($env_file_path);
-
     $env_lines = explode("\n", $env_content);
 
     foreach ($env_lines as $line) {
-        list($name, $value) = explode('=', $line, 2);
+        $line = trim($line);
 
-        if ($name && $value) {
+        if (!empty($line) && strpos($line, '=') !== false) {
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
             putenv("$name=$value");
             $_ENV[$name] = $value;
         }
